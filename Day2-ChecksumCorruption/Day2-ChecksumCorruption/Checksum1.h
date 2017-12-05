7a48c948c6353aacebc79d0e3e583412afc5984a
CARLES-MOLINS <carmoldu@hotmail.com>
2017-12-05 20:26:31 +0100

    Day 1 and 2

---
+++ Day2-ChecksumCorruption/Day2-ChecksumCorruption/Checksum1.h
@@ -0,0 +1,15 @@
+//#pragma once
+
+#ifndef CHECKSUM1
+#define CHECKSUM1
+
+int checksumCorruption1();
+
+#endif
+
+#ifndef CHECKSUM2
+#define CHECKSUM2
+
+int checksumCorruption2();
+
+#endif
---
+++ Day2-ChecksumCorruption/Day2-ChecksumCorruption/Source.cpp
@@ -0,0 +1,15 @@
+#include "Checksum1.h"
+
+int main() {
+
+
+
+	//DAY 2
+	checksumCorruption1();
+	checksumCorruption2();
+
+
+
+
+	return 0;
+}
